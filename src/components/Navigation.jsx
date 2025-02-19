0b7e17294d691c1caae589f1add400420651e878
Abdirashiid bile <abdirashiidbile5@gmail.com>
2025-02-19 13:26:59 +0300

    add profile github

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Navigation = () => {
   return (
@@ -10,6 +12,10 @@ const Navigation = () => {
           <Link to="/about" className="nav-link">About</Link>
           <Link to="/projects" className="nav-link">Projects</Link>
           <Link to="/contact" className="nav-link">Contact</Link>
+          <a href="https://github.com/Raash12" className="nav-link" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} style={{ marginRight: '5px' }} />
+            GitHub
+          </a>
         </div>
       </div>
     </nav>
